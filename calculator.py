168a332acdf09fa34228bd2727d352de561ec0b2
semodi <sebastian-dick@gmx.net>
2017-10-04 10:31:51 -0400

    added multiplication

--- calculator.py
+++ calculator.py
@@ -9,4 +9,11 @@ def add(arg1, arg2):
 
     return arg1 + arg2 
 
+def multiply(arg1, arg2):
+     
+    if float(arg1) != arg1 or float(arg2) != arg2:
+        raise Exception("Provided arguments not numerical!")
+
+    return arg1 * arg2 
+
 
--- test_calc.py
+++ test_calc.py
@@ -8,3 +8,7 @@ def test_add():
     assert(calc.add(2, -4) == -2)
     assert(abs(calc.add(1.2, 2000.4) - 2001.6) < 1e-10)
   
+
+def test_mult():
+    
+    assert(calc.multiply(2,3) == 6)
