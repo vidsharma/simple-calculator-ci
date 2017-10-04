2ba39d0915780f746c5982697ac549a6fd74d9b9
semodi <sebastian-dick@gmx.net>
2017-10-04 10:27:26 -0400

    first commit

---
+++ calculator.py
@@ -0,0 +1,12 @@
+"""
+Module that implements simple arithmetic operations
+"""
+
+def add(arg1, arg2):
+    
+    if float(arg1) != arg1 or float(arg2) != arg2:
+        raise Exception("Provided arguments not numerical!")
+
+    return arg1 + arg2 
+
+
