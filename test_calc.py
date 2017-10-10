672415871d8052a7ccfe2c826f899b0042cf600e
semodi <sebastian-dick@gmx.net>
2017-10-10 10:45:28 -0400

    parametrize and expected exception in pytest

--- test_calc.py
+++ test_calc.py
@@ -9,6 +9,15 @@ def test_add():
     assert(abs(calc.add(1.2, 2000.4) - 2001.6) < 1e-10)
   
 
-def test_mult():
-    
-    assert(calc.multiply(2,3) == 6)
+
+@pytest.mark.parametrize("arg1,arg2,out",
+    [ (2,3,6),
+    (0.1,6,pytest.approx(0.6)),
+    (-4,5,-20),
+    (0,123,0)])
+
+def test_mult(arg1, arg2, out):
+    assert(calc.multiply(arg1, arg2) == out)
+    with pytest.raises(Exception,
+             message = 'Exception for non-numerical argument'):
+        calc.multiply('NaN',0)
