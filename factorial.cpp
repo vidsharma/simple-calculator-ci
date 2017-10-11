34af7df9f713a9060b05ecbd4f25b47837e47527
semodi <sebastian-dick@gmx.net>
2017-10-11 11:12:07 -0400

    deleted code for presentation

--- factorial.cpp
+++ factorial.cpp
@@ -2,12 +2,10 @@
 #include "catch.hpp"
 
 unsigned int Factorial( unsigned int number ) {
-    if (number == 0) return 1;
     return number <= 1 ? number : Factorial(number-1)*number;
 }
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(0) == 1 );
     REQUIRE( Factorial(1) == 1 );
     REQUIRE( Factorial(2) == 2 );
     REQUIRE( Factorial(3) == 6 );
--- test_calc.py
+++ test_calc.py
@@ -4,20 +4,10 @@ import calculator as calc
 def test_add():
 
     assert(calc.add(2, 4) == 6)
-    assert(calc.add(0, 4) == 4)
-    assert(calc.add(2, -4) == -2)
     assert(abs(calc.add(1.2, 2000.4) - 2001.6) < 1e-10)
   
 
 
-@pytest.mark.parametrize("arg1,arg2,out",
-    [ (2,3,6),
-    (0.1,6,pytest.approx(0.6)),
-    (-4,5,-20),
-    (0,123,0)])
 
-def test_mult(arg1, arg2, out):
-    assert(calc.multiply(arg1, arg2) == out)
-    with pytest.raises(Exception,
-             message = 'Exception for non-numerical argument'):
-        calc.multiply('NaN',0)
+def test_mult():
+    pass
