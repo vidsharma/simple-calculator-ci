05a9430d37af0dfe493b02b3598d9c671fa0d38d
semodi <sebastian-dick@gmx.net>
2017-10-12 09:52:13 -0400

    changed test_calc.py

--- python/test_calc.py
+++ python/test_calc.py
@@ -4,10 +4,8 @@ import calculator as calc
 def test_add():
 
     assert(calc.add(2, 4) == 6)
-    assert(abs(calc.add(1.2, 2000.4) - 2001.6) < 1e-10)
+    assert(calc.add(1.2, 2000.4) == 2001.6)
   
-
-
 
 def test_mult():
     pass
